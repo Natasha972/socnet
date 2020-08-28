856e998cd22a76a1fbb21c0212d219e72503eb91
Natalia <n.krasheninina13@gmail.com>
2020-08-28 07:26:51 +0300

    replace attribution

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -8,6 +8,13 @@ function Posts(props) {
       {props.posts.map(
         (post)=> <PostContainer key={post.id} post={post} user={props.user} deletePost={props.deletePost} addLike={props.addLikeThunkCreator}/>)
       }
+      <div className="attribution">
+        Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </div>
+
     </div>
   );
 }
